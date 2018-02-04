abf6e57d44496f1e82dba84679d00bb234f63123
yashrajb <basanyash627@gmail.com>
2018-02-04 01:26:24 -0800

    edited app.js deleted some unessecary datas

--- src/app.js
+++ src/app.js
@@ -10,16 +10,6 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill',amount:30000 }));
-
-
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
